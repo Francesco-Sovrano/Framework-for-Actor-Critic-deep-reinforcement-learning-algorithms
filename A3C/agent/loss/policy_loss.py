Fix: Numerical error in entropy functions. Added speed control to car controller. Improvements to log system.
Fix: Numerical error in entropy functions.
Added speed control to car controller.
Improvements to log system.

A3C/agent/loss/policy_loss.py
@@ -9,9 +9,17 @@ import numpy as np
 import options
 flags = options.get()
 
-from agent.loss.loss import Loss
+class PolicyLoss(object):
 
-class PolicyLoss(Loss):
+	def __init__(self, cliprange, policy, old_policy, cross_entropy, old_cross_entropy, advantage, entropy, entropy_beta):
+		self.cliprange = cliprange
+		self.policy = policy
+		self.old_policy = old_policy
+		self.cross_entropy = cross_entropy
+		self.old_cross_entropy = old_cross_entropy
+		self.advantage = advantage
+		self.entropy = entropy
+		self.entropy_beta = entropy_beta
 		
 	def get(self):
 		if flags.policy_loss == 'vanilla':
@@ -20,73 +28,30 @@ class PolicyLoss(Loss):
 			return self.ppo()
 		elif flags.policy_loss == 'averagePPO':
 			return self.average_ppo()
-		elif flags.policy_loss == 'openaiPPO': # openAI implementation
-			return self.openai_ppo()
 			
-	def get_log_policy(self, policy):
-		return tf.log(tf.clip_by_value(policy, 1e-20, 1.0)) # Avoid NaN with clipping when value in pi becomes zero
-	
-	def get_action_log_policy(self, log_policy, action):
-		return tf.reduce_sum(tf.multiply(log_policy, action), reduction_indices=1 if len(self.policy.get_shape()) < 3 else [1,2])
-		
 	def vanilla(self):
-		# Avoid NaN with clipping when value in pi becomes zero
-		log_policy = self.get_log_policy(self.policy)
-		# Policy entropy
-		entropy = -tf.reduce_sum(self.policy * log_policy, reduction_indices=1 if len(self.policy.get_shape()) < 3 else [1,2])
-		# Policy loss (output)
-		return -tf.reduce_sum( self.get_action_log_policy(log_policy, self.action) * self.advantage + entropy * self.entropy_beta )
+		entropy = tf.reduce_sum(self.entropy)
+		policy_loss = -tf.reduce_sum( tf.multiply(self.cross_entropy,self.advantage) )
+		return policy_loss - tf.multiply(entropy,self.entropy_beta)
 		
 	def ppo(self):
 		# Schulman, John, et al. "Proximal policy optimization algorithms." arXiv preprint arXiv:1707.06347 (2017).
-		# old
-		old_log_policy = self.get_log_policy(self.old_policy)
-		old_action_log_policy = self.get_action_log_policy(old_log_policy, self.action)
-		# new
-		log_policy = self.get_log_policy(self.policy)
-		action_log_policy = self.get_action_log_policy(log_policy, self.action)
-		# ratio
-		ratio = tf.exp(action_log_policy - old_action_log_policy)
+		# Ratio
+		ratio = tf.exp(self.old_cross_entropy - self.cross_entropy)
 		clipped_ratio = tf.clip_by_value(ratio, 1.0 - self.cliprange, 1.0 + self.cliprange)
-		# entropy
-		entropy = -tf.reduce_sum(self.policy * log_policy, reduction_indices=1 if len(self.policy.get_shape()) < 3 else [1,2])
+		# Entropy
+		entropy = tf.reduce_sum(self.entropy)
 		# Policy loss
-		return -tf.reduce_sum( tf.minimum(ratio, clipped_ratio) * self.advantage + entropy * self.entropy_beta )
+		policy_loss = -tf.reduce_sum(tf.multiply(tf.minimum(ratio, clipped_ratio),self.advantage))
+		return policy_loss - tf.multiply(entropy,self.entropy_beta)
 				
 	def average_ppo(self):
 		# Schulman, John, et al. "Proximal policy optimization algorithms." arXiv preprint arXiv:1707.06347 (2017).
-		# old
-		old_log_policy = self.get_log_policy(self.old_policy)
-		old_action_log_policy = self.get_action_log_policy(old_log_policy, self.action)
-		# new
-		log_policy = self.get_log_policy(self.policy)
-		action_log_policy = self.get_action_log_policy(log_policy, self.action)
-		# ratio
-		ratio = tf.exp(action_log_policy - old_action_log_policy)
+		# Ratio
+		ratio = tf.exp(self.old_cross_entropy - self.cross_entropy)
 		clipped_ratio = tf.clip_by_value(ratio, 1.0 - self.cliprange, 1.0 + self.cliprange)
-		# entropy
-		entropy = -tf.reduce_sum(self.policy * log_policy, reduction_indices=1 if len(self.policy.get_shape()) < 3 else [1,2])
+		# Entropy
+		entropy = tf.reduce_mean(self.entropy)
 		# Policy loss
-		# self.advantage = (self.advantage - tf.reduce_mean(self.advantage)) / (self.reduce_std(self.advantage) + 1e-8)
-		return -tf.reduce_mean( tf.minimum(ratio, clipped_ratio) * self.advantage + entropy * self.entropy_beta )
-		
-	def openai_ppo(self):
-		# Schulman, John, et al. "Proximal policy optimization algorithms." arXiv preprint arXiv:1707.06347 (2017).
-		def log_pac(policy, action):
-			mean = tf.reduce_mean(input_tensor=policy, axis=1, keepdims=True)
-			std = self.reduce_std(input_tensor=policy, axis=1, keepdims=True)
-			logstd = tf.log(std)
-			return 0.5 * tf.reduce_sum(tf.square((action - mean) / std), axis=-1) \
-               + 0.5 * np.log(2.0 * np.pi) * tf.to_float(tf.shape(action)[-1]) \
-               + tf.reduce_sum(logstd, axis=-1)
-		# old
-		old_action_log_policy = log_pac(self.old_policy, self.action)
-		# new
-		action_log_policy = log_pac(self.policy, self.action)
-		# ratio
-		ratio = tf.exp(action_log_policy - old_action_log_policy)
-		clipped_ratio = tf.clip_by_value(ratio, 1.0 - self.cliprange, 1.0 + self.cliprange)
-		# entropy
-		entropy = -tf.reduce_sum(tf.log(self.reduce_std(input_tensor=self.policy, axis=1)) + .5 * np.log(2.0 * np.pi * np.e), axis=-1)
-		# Policy loss
-		return -tf.reduce_mean( tf.minimum(ratio, clipped_ratio) * self.advantage + entropy * self.entropy_beta )
+		policy_loss = -tf.reduce_mean(tf.multiply(tf.minimum(ratio, clipped_ratio),self.advantage))
+		return policy_loss - tf.multiply(entropy,self.entropy_beta)

A3C/agent/manager/basic_manager.py
@@ -149,8 +149,10 @@ class BasicManager(object):
 		if flags.clip_reward:
 			reward = np.clip(reward, flags.min_reward, flags.max_reward)
 		
-		self.batch.add_agent_action(agent_id, state, concat, agent.get_action_vector(action), reward, value, policy, lstm_state)
-		return new_state, policy, value, action, reward, terminal
+		action_vector = agent.get_action_vector(action)
+		cross_entropy, entropy = agent.run_cross_entropy(actions=[action_vector],states=[state],concats=[concat],lstm_state=lstm_state)
+		self.batch.add_agent_action(agent_id, state, concat, action_vector, cross_entropy, reward, value, policy, lstm_state)
+		return new_state, policy, value, action, reward, terminal, cross_entropy, entropy
 					
 	def compute_cumulative_reward(self, batch):
 		# prepare batch
@@ -179,6 +181,7 @@ class BasicManager(object):
 		states = batch.states
 		concats = batch.concats
 		actions = batch.actions
+		cross_entropies = batch.cross_entropies
 		values = batch.values
 		policies = batch.policies
 		rewards = batch.rewards
@@ -200,6 +203,7 @@ class BasicManager(object):
 				model.train(
 					states=states[i], concats=concats[i],
 					actions=actions[i], values=values[i],
+					cross_entropies=cross_entropies[i],
 					policies=policies[i], rewards=rewards[i],
 					discounted_cumulative_rewards=dcr[i],
 					generalized_advantage_estimators=gae[i],

A3C/agent/network/base_actor_critic_network.py
@@ -24,8 +24,8 @@ class BaseAC_Network(object):
 		self._session = session
 		self._id = id # model id
 		self._device = device # gpu or cpu
-		self._policy_length = action_shape[0] # the dimension of the policy vector
-		self._policy_depth = action_shape[1] if len(action_shape) > 1 else 0 # dimension of the softmax: 0 for none, 1 if you want a single softmax for the whole policy, 2 or more if you want a softmax (with dimension self._policy_depth) for any policy element
+		self.policy_length = action_shape[0] # the dimension of the policy vector
+		self.policy_depth = action_shape[1] if len(action_shape) > 1 else 0 # dimension of the softmax: 0 for none, 1 if you want a single softmax for the whole policy, 2 or more if you want a softmax (with dimension self.policy_depth) for any policy element
 		self._concat_size = concat_size # the size of the vector concatenated with the CNN output before entering the LSTM
 		self._state_shape = state_shape # the shape of the input
 		# lstm units
@@ -33,14 +33,26 @@ class BaseAC_Network(object):
 		# create the whole A3C network
 		self._create_network()
 		
-	def _lstm_state_placeholder(self):
-		return tf.placeholder(tf.float32, [1, self._lstm_units])
+	def _lstm_state_placeholder(self, name=None):
+		return tf.placeholder(dtype=tf.float32, shape=[1, self._lstm_units], name=name)
 		
-	def _state_placeholder(self):
-		return tf.placeholder(tf.float32, np.concatenate([[None], self._state_shape], 0))
+	def _state_placeholder(self, name=None):
+		return tf.placeholder(dtype=tf.float32, shape=np.concatenate([[None], self._state_shape], 0), name=name)
 		
-	def _concat_placeholder(self):
-		return tf.placeholder(tf.float32, [None, self._concat_size])
+	def _policy_placeholder(self, name=None):
+		if self.policy_depth < 2:
+			return tf.placeholder(dtype=tf.float32, shape=[None,self.policy_length], name=name)
+		else:
+			return tf.placeholder(dtype=tf.float32, shape=[None,self.policy_length,self.policy_depth], name=name)
+		
+	def _value_placeholder(self, name=None):
+		return tf.placeholder(dtype=tf.float32, shape=[None,1], name=name)
+		
+	def _concat_placeholder(self, name=None):
+		return tf.placeholder(dtype=tf.float32, shape=[None, self._concat_size], name=name)
+		
+	def _reward_prediction_target_placeholder(self, name=None):
+		return tf.placeholder(dtype=tf.float32, shape=[1,3], name=name)
 	
 	def _create_network(self):
 		scope_name = "net_{0}".format(self._id)
@@ -54,26 +66,26 @@ class BaseAC_Network(object):
 	def _build_base(self):
 		print( "Building network {}".format(self._id) )
 		# [Input]
-		self._input = self._state_placeholder()
-		print( "    [{}]Input shape: {}".format(self._id, self._input.get_shape()) )
+		self.state_batch = self._state_placeholder("state")
+		print( "    [{}]Input shape: {}".format(self._id, self.state_batch.get_shape()) )
 		# [Concatenation]
-		self._concat = self._concat_placeholder()
+		self._concat = self._concat_placeholder("concat")
 		print( "    [{}]Concatenation shape: {}".format(self._id, self._concat.get_shape()) )
 		# [CNN tower]
-		tower = self._convolutive_layers(self._input)
+		tower = self._convolutive_layers(self.state_batch)
 		print( "    [{}]Tower shape: {}".format(self._id, tower.get_shape()) )
 		# [LSTM]
 		lstm, self._lstm_state = self._lstm_layers(tower, self._concat)
 		print( "    [{}]LSTM shape: {}".format(self._id, lstm.get_shape()) )
 		# [Policy]
-		self._policy = self._policy_layers(lstm)
-		print( "    [{}]Policy shape: {}".format(self._id, self._policy.get_shape()) )
+		self.policy_batch = self._policy_layers(lstm)
+		print( "    [{}]Policy shape: {}".format(self._id, self.policy_batch.get_shape()) )
 		# [Value]
-		self._value	= self._value_layers(lstm)
-		print( "    [{}]Value shape: {}".format(self._id, self._value.get_shape()) )
+		self.value_batch = self._value_layers(lstm)
+		print( "    [{}]Value shape: {}".format(self._id, self.value_batch.get_shape()) )
 		
 	def _build_reward_prediction(self):
-		self._reward_prediction_states = self._state_placeholder()
+		self._reward_prediction_states = self._state_placeholder("reward_prediction_state")
 		output = self._convolutive_layers(self._reward_prediction_states, reuse=True)
 		output = tf.layers.flatten(output)
 		output = tf.layers.dense(inputs=output, units=3, activation=None, kernel_initializer=tf.initializers.variance_scaling)
@@ -102,7 +114,8 @@ class BaseAC_Network(object):
 
 			# self._lstm_cell = tf.contrib.model_pruning.MaskedBasicLSTMCell(num_units=self._lstm_units, forget_bias=1.0, state_is_tuple=True, activation=None)
 			self._lstm_cell = tf.contrib.rnn.LSTMCell(num_units=self._lstm_units)
-			self._initial_lstm_state = tf.contrib.rnn.LSTMStateTuple(self._lstm_state_placeholder(), self._lstm_state_placeholder())
+			self._initial_lstm_state = tf.contrib.rnn.LSTMStateTuple(self._lstm_state_placeholder("lstm_tuple_1"), self._lstm_state_placeholder("lstm_tuple_2"))
+			self._empty_lstm_state = (np.zeros([1,self._lstm_units]),np.zeros([1,self._lstm_units]))
 			lstm_outputs, lstm_state = tf.nn.dynamic_rnn(cell=self._lstm_cell, inputs=input, initial_state=self._initial_lstm_state, sequence_length=step_size, time_major = False, scope = scope)
 			
 			# Dropout: https://www.nature.com/articles/s41586-018-0102-6
@@ -113,51 +126,85 @@ class BaseAC_Network(object):
 			
 	def _policy_layers(self, input, reuse=False): # Policy (output)
 		with tf.variable_scope("base_policy{0}".format(self._id), reuse=reuse) as scope:
-			if self._policy_depth < 2:
-				input = tf.layers.dense(inputs=input, units=self._policy_length, activation=None, kernel_initializer=tf.initializers.variance_scaling)
-				return input if self._policy_depth == 0 else tf.nn.softmax(input)
+			if self.policy_depth < 2:
+				input = tf.layers.dense(inputs=input, units=self.policy_length, activation=None, kernel_initializer=tf.initializers.variance_scaling)
+				self.logits = input
+				return tf.nn.softmax(self.logits) if self.policy_depth > 0 else self.logits
 			else:
 				policy = []
-				for i in range(self._policy_depth):
-					p = tf.layers.dense(inputs=input, units=self._policy_length, activation=None, kernel_initializer=tf.initializers.variance_scaling)
+				for i in range(self.policy_depth):
+					p = tf.layers.dense(inputs=input, units=self.policy_length, activation=None, kernel_initializer=tf.initializers.variance_scaling)
 					p = tf.expand_dims(p, axis=-1)
 					policy.append(p)
-				return tf.contrib.layers.softmax(tf.concat(policy, -1))
+				self.logits = tf.concat(policy, -1)
+				return tf.contrib.layers.softmax(self.logits)
 
 	def _value_layers(self, input, reuse=False): # Value (output)
 		with tf.variable_scope("base_value{0}".format(self._id), reuse=reuse) as scope:
 			input = tf.layers.dense( inputs=input, units=1, activation=None, kernel_initializer=tf.initializers.variance_scaling )
 			return tf.reshape(input, [-1]) # flatten output
-
+		
+	def get_cross_entropy(self, onehot_labels, logits):
+		# p
+		a0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
+		ea0 = tf.exp(a0)
+		z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
+		p0 = ea0 / z0
+		# q
+		a1 = onehot_labels - tf.reduce_max(onehot_labels, axis=-1, keepdims=True)
+		ea1 = tf.exp(a1)
+		z1 = tf.reduce_sum(ea1, axis=-1, keepdims=True)
+		p1 = ea1 / z1
+		return tf.reduce_sum( tf.multiply(p1, (tf.log(z0) - a0)), axis=-1)
+			
+	def get_entropy(self, logits):
+		a0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
+		ea0 = tf.exp(a0)
+		z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
+		p0 = ea0 / z0
+		return tf.reduce_sum( tf.multiply(p0, (tf.log(z0) - a0)), axis=-1)
+			
 	def prepare_loss(self):
 		with tf.device(self._device):
-			# Batch state values
-			self.old_value_batch = tf.placeholder(tf.float32, [None,1])
-			# Taken action (input for policy) and Batch policies
-			if self._policy_depth < 2:
-				self.action_batch = tf.placeholder(tf.float32, [None, self._policy_length]) # do not normalize a hot-vector
-				self.old_policy_batch = tf.placeholder(tf.float32, [None, self._policy_length]) # do not normalize a probability distribution
-			else:
-				self.action_batch = tf.placeholder(tf.float32, [None, self._policy_length, self._policy_depth]) # do not normalize a hot-vector
-				self.old_policy_batch = tf.placeholder(tf.float32, [None, self._policy_length, self._policy_depth]) # do not normalize a probability distribution
-			# Advantage (R-V) (input for policy)
-			self.advantage_batch = tf.placeholder(tf.float32, [None,1])
-			# R (input for value target)
-			self.cumulative_reward_batch = tf.placeholder(tf.float32, [None,1])
-			# Loss
-			self.policy_loss = PolicyLoss(self.clip, self._policy, self.old_policy_batch, self._value, self.old_value_batch, self.action_batch, self.advantage_batch, self.cumulative_reward_batch, self.entropy_beta)
-			self.value_loss = ValueLoss(self.clip, self._policy, self.old_policy_batch, self._value, self.old_value_batch, self.action_batch, self.advantage_batch, self.cumulative_reward_batch, self.entropy_beta)
-			
+			# Batch placeholders
+			self.action_batch = self._policy_placeholder("action")
+			self.old_policy_batch = self._policy_placeholder("old_policy")
+			self.old_value_batch = self._value_placeholder("old_value")
+			self.advantage_batch = self._value_placeholder("advantage")
+			self.cumulative_reward_batch = self._value_placeholder("cumulative_reward")
+			self.entropy_batch = self.get_entropy(logits=self.logits)
+			self.cross_entropy_batch = tf.losses.softmax_cross_entropy(onehot_labels=self.action_batch, logits=self.logits) if self.policy_depth > 0 else self.get_cross_entropy(onehot_labels=self.action_batch, logits=self.logits)
+			# The following alternative is: (i) less numerically stable (since the softmax may compute much larger values) and (ii) less efficient (since some redundant computation would happen in the backprop)
+			# log_batch = tf.log(tf.clip_by_value(self.logits, 1e-8, 1.0)) # Avoid NaN with clipping when value in tensor becomes zero
+			# return tf.reduce_sum(tf.multiply(log_batch, self.action_batch), reduction_indices=1)
+			self.old_cross_entropy_batch = self._value_placeholder("old_cross_entropy")
+			# Build losses
+			self.policy_loss = PolicyLoss(
+				cliprange=self.clip,
+				policy=self.policy_batch,
+				old_policy=self.old_policy_batch,
+				cross_entropy=self.cross_entropy_batch,
+				old_cross_entropy=self.old_cross_entropy_batch,
+				advantage=self.advantage_batch,
+				entropy=self.entropy_batch,
+				entropy_beta=self.entropy_beta
+			)
+			self.value_loss = ValueLoss(
+				cliprange=self.clip, 
+				value=self.value_batch, 
+				old_value=self.old_value_batch, 
+				reward=self.cumulative_reward_batch
+			)
+			# Compute total loss
 			self.total_loss = self.policy_loss.get() + flags.value_coefficient*self.value_loss.get()
 			if self.predict_reward:
 				self.total_loss += self.reward_prediction_loss()
 			
 	def reward_prediction_loss(self):
 		# reward prediction target. one hot vector
-		self.reward_prediction_target = tf.placeholder(tf.float32, [1,3]) # it's a hot-vector, do not normalize it
+		self.reward_prediction_target = self._reward_prediction_target_placeholder("reward_prediction_target")
 		# Reward prediction loss (output)
-		clipped_rp = tf.clip_by_value(self._reward_prediction, 1e-20, 1.0)
-		return -tf.reduce_sum(self.reward_prediction_target * tf.log(clipped_rp))
+		return -self.get_cross_entropy(onehot_labels=self.reward_prediction_target, logits=self._reward_prediction)
 
 	def bind_sync(self, src_network):
 		src_vars = src_network.get_vars()
@@ -196,64 +243,74 @@ class BaseAC_Network(object):
 	def get_action_vector(self, action=None): # transform action into a 1-hot-vector
 		if type(action) not in [list,tuple]:
 			action = [action]
-		if self._policy_depth < 2:
-			hot_vector = np.zeros([self._policy_length])
+		if self.policy_depth < 2:
+			hot_vector = np.zeros([self.policy_length])
 			for i in range(len(action)):
-				hot_vector[action[i]] = 1.0
+				hot_vector[action[i]] = 1
 			return hot_vector
 		else:
-			hot_vector = np.zeros([self._policy_length, self._policy_depth])
-			for i in range(self._policy_length):
-				hot_vector[i][action[i]] = 1.0
+			hot_vector = np.zeros([self.policy_length, self.policy_depth])
+			for i in range(self.policy_length):
+				hot_vector[i][action[i]] = 1
 			return hot_vector
 		
-	def get_empty_lstm_state(self):
-		return (np.zeros([1, self._lstm_units]), np.zeros([1, self._lstm_units]))
+	def run_cross_entropy(self, actions, states, concats=None, lstm_state=None):
+		feed_dict = { 
+				self.action_batch : actions,
+				self.state_batch : states,
+				self._initial_lstm_state: lstm_state if lstm_state is not None else self._empty_lstm_state
+			}
+		if self._concat_size > 0:
+			feed_dict.update( { self._concat : concats } )
+		entropy_batch, cross_entropy_batch, _, _, _ = self._session.run(fetches=[self.entropy_batch, self.cross_entropy_batch, self.policy_batch, self.value_batch, self._lstm_state], feed_dict=feed_dict, options=tf.RunOptions.NO_TRACE)
+		return cross_entropy_batch, entropy_batch
 		
 	def run_policy_and_value(self, states, concats=None, lstm_state=None):
 		feed_dict = { 
-				self._input : states,
-				self._initial_lstm_state: lstm_state if lstm_state is not None else self.get_empty_lstm_state()
+				self.state_batch : states,
+				self._initial_lstm_state: lstm_state if lstm_state is not None else self._empty_lstm_state
 			}
 		if self._concat_size > 0:
 			feed_dict.update( { self._concat : concats } )
 		# return policies, values, lstm_state
-		return self._session.run(fetches=[self._policy, self._value, self._lstm_state], feed_dict=feed_dict, options=tf.RunOptions.NO_TRACE)
+		return self._session.run(fetches=[self.policy_batch, self.value_batch, self._lstm_state], feed_dict=feed_dict, options=tf.RunOptions.NO_TRACE)
 				
 	def run_value(self, states, concats=None, lstm_state=None):
 		feed_dict = { 
-				self._input : states, 
-				self._initial_lstm_state: lstm_state if lstm_state is not None else self.get_empty_lstm_state()
+				self.state_batch : states, 
+				self._initial_lstm_state: lstm_state if lstm_state is not None else self._empty_lstm_state
 			}
 		if self._concat_size > 0:
 			feed_dict.update( { self._concat : concats } )
 		# return values, lstm_state
-		return self._session.run(fetches=[self._value, self._lstm_state], feed_dict=feed_dict, options=tf.RunOptions.NO_TRACE)
+		return self._session.run(fetches=[self.value_batch, self._lstm_state], feed_dict=feed_dict, options=tf.RunOptions.NO_TRACE)
 				
-	def train(self, states, actions, rewards, values, policies, discounted_cumulative_rewards, generalized_advantage_estimators, lstm_state=None, concats=None, reward_prediction_states=None, reward_prediction_target=None):
+	def train(self, states, actions, rewards, values, policies, cross_entropies, discounted_cumulative_rewards, generalized_advantage_estimators, lstm_state=None, concats=None, reward_prediction_states=None, reward_prediction_target=None):
 		self.train_count += len(states)
-		feed_dict = self.build_feed(states, actions, rewards, values, policies, discounted_cumulative_rewards, generalized_advantage_estimators, lstm_state, concats, reward_prediction_states, reward_prediction_target)
+		feed_dict = self.build_feed(states, actions, rewards, values, policies, cross_entropies, discounted_cumulative_rewards, generalized_advantage_estimators, lstm_state, concats, reward_prediction_states, reward_prediction_target)
 		self._session.run(fetches=self.train_op, feed_dict=feed_dict, options=tf.RunOptions.NO_TRACE) # Calculate gradients and copy them to global network
 		
-	def build_feed(self, states, actions, rewards, values, policies, discounted_cumulative_rewards, generalized_advantage_estimators, lstm_state=None, concat=None, reward_prediction_states=None, reward_prediction_target=None):
+	def build_feed(self, states, actions, rewards, values, policies, cross_entropies, discounted_cumulative_rewards, generalized_advantage_estimators, lstm_state, concats, reward_prediction_states, reward_prediction_target):
+		cross_entropies = np.reshape(cross_entropies,[-1,1])
 		values = np.reshape(values,[-1,1])
 		if flags.use_GAE: # Schulman, John, et al. "High-dimensional continuous control using generalized advantage estimation." arXiv preprint arXiv:1506.02438 (2015).
 			advantages = np.reshape(generalized_advantage_estimators,[-1,1])
 			cumulative_rewards = advantages + values
 		else:
 			cumulative_rewards = np.reshape(discounted_cumulative_rewards,[-1,1])
-			advantages = cumulative_rewards - values			
+			advantages = cumulative_rewards - values
 		feed_dict={
-					self._input: states,
+					self.state_batch: states,
 					self.action_batch: actions,
 					self.cumulative_reward_batch: cumulative_rewards,
 					self.advantage_batch: advantages,
 					self.old_value_batch: values,
-					self.old_policy_batch: policies,
-					self._initial_lstm_state: lstm_state if lstm_state is not None else self.get_empty_lstm_state()
+					# self.old_policy_batch: policies,
+					self.old_cross_entropy_batch: cross_entropies,
+					self._initial_lstm_state: lstm_state if lstm_state is not None else self._empty_lstm_state
 				}
 		if self._concat_size > 0:
-			feed_dict.update( {self._concat : concat} )
+			feed_dict.update( {self._concat : concats} )
 		if self.predict_reward:
 			feed_dict.update( {
 				self._reward_prediction_states: reward_prediction_states,

A3C/environment/car_controller_environment.py
@@ -21,16 +21,21 @@ class CarControllerEnvironment(Environment):
 		return (1,6,2)
 
 	def get_action_shape(self):
-		return (1,) # steering angle, continuous control
+		return (2,) # steering angle, continuous control without softmax
 	
 	def __init__(self, thread_index):
 		Environment.__init__(self)
 		self.thread_index = thread_index
 		self.max_distance_to_path = 0.1 # meters
 		self.max_noise_diameter = 0.4 # diameter in meters
-		self.speed = 0.1 # meters per step (assuming a step each 0.1 seconds -> 3.6km/h)
-		self.max_angle_degrees = 30
-		self.max_angle_radians = convert_degree_to_radians(self.max_angle_degrees)
+		self.min_speed = 0.05 # meters per step (assuming a step each 0.1 seconds -> 1.8km/h)
+		self.max_speed = 0.2 # meters per step (assuming a step each 0.1 seconds -> 7.2km/h)
+		self.max_speed_change_per_step = 0.01
+		self.gamma = 10
+		self.max_steering_degree = 30
+		self.max_compensation_degree = 15
+		self.max_steering_angle = convert_degree_to_radians(self.max_steering_degree)
+		self.max_compensation_angle = convert_degree_to_radians(self.max_compensation_degree)
 		self.positions_number = 100
 		self.max_step = self.positions_number
 		self.noise_per_step = self.max_noise_diameter/self.max_step
@@ -55,15 +60,19 @@ class CarControllerEnvironment(Environment):
 		
 	def reset(self):
 		self.path = self.build_random_path()
-		self.noisy_point = self.car_point = (0, 0) # car point and orientation are always expressed with respect to the initial point and orientation of the road fragment
+		self.noisy_point = self.car_point = (0,0) # car point and orientation are always expressed with respect to the initial point and orientation of the road fragment
+		self.car_angle = np.pi/2 + 0.01		
 		self.noisy_progress = self.car_progress = 0
 		self.noisy_waypoint = self.car_waypoint = 1
 		
+		self.speed = self.min_speed
+		self.steering_angle = 0		
 		self.cumulative_reward = 0
 		self.step = 0
-		self.last_action = 0
+		self.last_action = [0,0]
 		self.last_reward = 0
 		self.last_state = self.get_state(self.car_point, self.car_waypoint)
+		self.avg_speed_per_steps = 0
 		
 	def get_car_position_and_next_waypoint(self, car_point):
 		car_x, car_y = car_point
@@ -76,46 +85,59 @@ class CarControllerEnvironment(Environment):
 		next_waypoint = bisect_right(self.positions, car_position)
 		# print(car_position, next_waypoint)
 		return car_position, next_waypoint
-		
-	def compute_new_car_point(self, car_point, next_waypoint, compensation_angle):
-		# get baseline angle
-		xs, ys = self.path
-		relative_path, _ = get_relative_path_and_next_position_id(point=car_point, path=([xs[next_waypoint]], [ys[next_waypoint]]))
-		_, yc = relative_path		
-		baseline_angle = angle(self.positions[next_waypoint], self.U1, self.V1) + np.arctan(yc[0]/(self.speed*10)) # use the tangent to the next waypoint as default direction -> more stable results
-		car_angle = baseline_angle + compensation_angle # adjust the default direction using the compensation_angle
+
+	def compute_new_car_point_and_angle(self, car_point, car_angle, steering_angle, speed, add_noise=False):
+		# get new car angle
+		new_car_angle = car_angle + steering_angle
 		# change point
 		car_x, car_y = car_point
-		car_x += self.speed*np.cos(car_angle)
-		car_y += self.speed*np.sin(car_angle)
-		return (car_x, car_y)
+		car_x += speed*np.cos(new_car_angle)
+		car_y += speed*np.sin(new_car_angle)
+		if add_noise: # add noise to point
+			car_x += (2*np.random.random()-1)*self.noise_per_step
+			car_y += (2*np.random.random()-1)*self.noise_per_step
+		return (car_x, car_y), new_car_angle
+
+	def compute_new_steering_angle(self, speed, steering_angle, car_point, car_angle, compensation_angle, next_waypoint):
+		# get baseline angle
+		xs, ys = self.path
+		relative_path, _ = get_relative_path_and_next_waypoint(point=car_point, angle=car_angle, path=([xs[next_waypoint]], [ys[next_waypoint]]))
+		_, yc = relative_path
+		baseline_angle = angle(self.positions[next_waypoint], self.U1, self.V1) + np.arctan(yc[0]/(speed*self.gamma)) # use the tangent to the next waypoint as default direction -> more stable results
+		# get new angle
+		new_angle = compensation_angle + baseline_angle
+		# get steering angle
+		steering_angle += new_angle - car_angle
+		# clip steering angle in [-max_steering_angle, max_steering_angle]
+		return np.clip(steering_angle,-self.max_steering_angle,self.max_steering_angle)
 		
-	def get_noisy_point(self, point):
-		x, y = point
-		# add noise to point
-		x += (2*np.random.random()-1)*self.noise_per_step
-		y += (2*np.random.random()-1)*self.noise_per_step
-		return (x, y)
+	def compute_new_speed(self, speed, step_acceleration):
+		speed += step_acceleration
+		return np.clip(speed, self.min_speed, self.max_speed)
 
 	def process(self, policy):
-		policy_choice=0
-		action = np.clip(policy[0],0,1)
-		# get compensation angle
-		compensation_angle = (2*action-1)*self.max_angle_radians
+		policy_choice=[0,1]
+		action = [np.clip(policy[0],0,1),np.clip(policy[1],0,1)]
+		# compute new speed
+		step_acceleration = (2*action[1]-1)*self.max_speed_change_per_step
+		self.speed = self.compute_new_speed(speed=self.speed, step_acceleration=step_acceleration)
+		# compute new steering_angle
+		compensation_angle = (2*action[0]-1)*self.max_compensation_angle
+		self.steering_angle = self.compute_new_steering_angle(speed=self.speed, steering_angle=self.steering_angle, compensation_angle=compensation_angle, car_point=self.car_point, car_angle=self.car_angle, next_waypoint=self.car_waypoint)
 		# update perceived car point
-		self.car_point = self.compute_new_car_point(self.car_point, self.car_waypoint, compensation_angle)
+		self.car_point, self.car_angle = self.compute_new_car_point_and_angle(car_point=self.car_point, car_angle=self.car_angle, steering_angle=self.steering_angle, speed=self.speed)
 		# update real car point
-		self.noisy_point = self.get_noisy_point(self.compute_new_car_point(self.noisy_point, self.car_waypoint, compensation_angle)) # use perceived waypoint here!
+		self.noisy_point, _ = self.compute_new_car_point_and_angle(car_point=self.noisy_point, car_angle=self.car_angle, steering_angle=self.steering_angle, speed=self.speed, add_noise=True)
 		# update position and direction
 		car_position, car_waypoint = self.get_car_position_and_next_waypoint(self.car_point)
 		noisy_position, noisy_waypoint = self.get_car_position_and_next_waypoint(self.noisy_point)
 		# compute real reward
-		noisy_reward = self.get_reward(self.noisy_point, self.noisy_progress, noisy_position)
+		noisy_reward = self.get_reward(car_speed=self.speed, car_point=self.noisy_point, car_progress=self.noisy_progress, car_position=noisy_position)
 		if noisy_position > self.noisy_progress: # is moving toward next position
 			self.noisy_progress = noisy_position # progress update
 			self.noisy_waypoint = noisy_waypoint
 		# compute perceived reward
-		car_reward = self.get_reward(self.car_point, self.car_progress, car_position)
+		car_reward = self.get_reward(car_speed=self.speed, car_point=self.car_point, car_progress=self.car_progress, car_position=car_position)
 		if car_position > self.car_progress: # is moving toward next position
 			self.car_progress = car_position # progress update
 			self.car_waypoint = car_waypoint
@@ -127,24 +149,31 @@ class CarControllerEnvironment(Environment):
 		self.last_reward = car_reward
 		# update cumulative reward
 		self.cumulative_reward += noisy_reward
+		self.avg_speed_per_steps += self.speed
 		# update step
 		self.step += 1
 		terminal = self.is_terminal_position(self.car_waypoint) or self.is_terminal_position(self.noisy_waypoint) or self.step >= self.max_step
 		if terminal: # populate statistics
-			self.episodes.append( {"reward":self.cumulative_reward, "step": self.step, "completed": 1 if self.is_terminal_position(self.car_waypoint) else 0} )
+			stats = {
+				"avg_speed": self.avg_speed_per_steps/self.step,
+				"reward": self.cumulative_reward,
+				"step": self.step,
+				"completed": 1 if self.is_terminal_position(self.car_waypoint) else 0
+			}
+			self.episodes.append(stats)
 			if len(self.episodes) > flags.match_count_for_evaluation:
 				self.episodes.popleft()
 		return policy_choice, state, noisy_reward, terminal
 		
 	def get_concatenation(self):
-		return [self.last_action, self.last_reward]
+		return self.last_action + [self.last_reward]
 		
-	def get_reward(self, car_point, car_progress, car_position):
+	def get_reward(self, car_speed, car_point, car_progress, car_position):
 		if car_position > car_progress: # is moving toward next position
 			x, y = poly(car_position,self.U1), poly(car_position,self.V1)
 			distance = euclidean_distance(car_point,(x,y))
 			# distance = get_distance(self.positions[next_waypoint]) if next_waypoint < self.positions_number else get_distance(1)
-			return 1 - np.clip(distance/self.max_distance_to_path,0,1) # always in [0,1] # smaller distances to path give higher rewards
+			return car_speed*(1 - np.clip(distance/self.max_distance_to_path,0,1)) # always in [0,1] # smaller distances to path give higher rewards
 		return -0.1 # is NOT moving toward next position
 		
 	def get_state(self, car_point, next_waypoint):
@@ -162,14 +191,14 @@ class CarControllerEnvironment(Environment):
 		return state
 		
 	def get_screen(self):
-		relative_path, position_id = get_relative_path_and_next_waypoint(point=self.car_point, path=self.path)
+		relative_path, position_id = get_relative_path_and_next_waypoint(point=self.car_point, path=self.path, angle=self.car_angle)
 		xc, yc = relative_path
 		xct = xc[position_id:min(position_id+self.positions_number,2*self.positions_number)] if position_id < len(xc) else []
 		yct = yc[position_id:min(position_id+self.positions_number,2*self.positions_number)] if position_id < len(xc) else []
 		return (xct,yct)
 		
-	def get_frame_info(self, network, observation, policy, value, action, reward):
-		state_info = "reward={}, action={}, agent={}, value={}\n".format(reward, action, network.agent_id, value)
+	def get_frame_info(self, network, observation, policy, value, action, reward, cross_entropy, entropy):
+		state_info = "reward={}, speed={}, steering_angle={}, agent={}, value={}, cross_entropy={}, entropy={}\n".format(reward, self.speed, self.steering_angle, network.agent_id, value, cross_entropy, entropy)
 		policy_info = "policy={}\n".format(policy)
 		frame_info = { "log": state_info + policy_info }
 		if flags.save_episode_screen:
@@ -189,15 +218,18 @@ class CarControllerEnvironment(Environment):
 		result = {}
 		result["avg_reward"] = 0
 		result["avg_step"] = 0
+		result["avg_speed"] = 0
 		result["avg_completed"] = 0
 		count = len(self.episodes)
 		if count>0:
 			for e in self.episodes:
 				result["avg_reward"] += e["reward"]
 				result["avg_step"] += e["step"]
+				result["avg_speed"] += e["avg_speed"]
 				result["avg_completed"] += e["completed"]
 			result["avg_reward"] /= count
 			result["avg_step"] /= count
+			result["avg_speed"] /= count
 			result["avg_completed"] /= count
 		return result
 		
@@ -291,10 +323,10 @@ def norm(angle):
 def convert_degree_to_radians(degree):
 	return (degree/180)*np.pi
 	
-def get_relative_path_and_next_waypoint(point, path):
+def get_relative_path_and_next_waypoint(point, path, angle):
 	point_x, point_y = point
 	path_xs, path_ys = path
-	relative_path = shift_and_rotate(path_xs, path_ys, -point_x, -point_y, 0)
+	relative_path = shift_and_rotate(path_xs, path_ys, -point_x, -point_y, -angle)
 	xc, yc = relative_path
 	next_waypoint = 0
 	while next_waypoint < len(xc) and xc[next_waypoint] < 0:

A3C/environment/rogue_environment.py
@@ -48,17 +48,16 @@ class RogueEnvironment(environment.Environment):
 	def get_screen(self):
 		return self.game.get_screen()
 		
-	def get_frame_info(self, network, observation, policy, value, action, reward):
+	def get_frame_info(self, network, observation, policy, value, action, reward, cross_entropy, entropy):
 		# Screen
 		last_frame = self.game.get_frame(-2)	
-		state_info = "reward={}, passages={}, doors={}, below_player={}, agent={}, action={}, value={}\n".format( 
+		state_info = "reward={}, passages={}, doors={}, below_player={}, agent={}, action={}, value={}, cross_entropy={}, entropy={}\n".format( 
 			reward,
 			last_frame.get_tile_count("#"),
 			last_frame.get_tile_count("+"),
 			last_frame.get_tile_below_player(),
 			network.agent_id,
-			action,
-			value
+			action, value, cross_entropy, entropy
 		)
 		policy_info = "policy={}\n".format(policy)
 		# observation_info = "observation={}".format(np.array_str(observation.flatten()))