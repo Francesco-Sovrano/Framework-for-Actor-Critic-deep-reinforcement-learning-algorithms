46571279d1fabfa7f9f521370c21f93d8610d3ee
Cescoso <cesco.sovrano@gmail.com>
2018-08-07 18:50:16 +0200

    Fix: wrong entropy functions for continuous control.
    Fix: wrong entropy functions for continuous control.

--- A3C/agent/network/base_actor_critic_network.py
+++ A3C/agent/network/base_actor_critic_network.py
@@ -88,8 +88,7 @@ class BaseAC_Network(object):
 		self._reward_prediction_states = self._state_placeholder("reward_prediction_state")
 		output = self._convolutive_layers(self._reward_prediction_states, reuse=True)
 		output = tf.layers.flatten(output)
-		output = tf.layers.dense(inputs=output, units=3, activation=None, kernel_initializer=tf.initializers.variance_scaling)
-		self._reward_prediction = tf.nn.softmax(output)
+		self.reward_prediction_logits = tf.layers.dense(inputs=output, units=3, activation=None, kernel_initializer=tf.initializers.variance_scaling)
 		
 	def _convolutive_layers(self, input, reuse=False):
 		with tf.variable_scope("base_conv{0}".format(self._id), reuse=reuse) as scope:
@@ -128,55 +127,61 @@ class BaseAC_Network(object):
 		with tf.variable_scope("base_policy{0}".format(self._id), reuse=reuse) as scope:
 			if self.policy_depth < 2:
 				input = tf.layers.dense(inputs=input, units=self.policy_length, activation=None, kernel_initializer=tf.initializers.variance_scaling)
-				self.logits = input
-				return tf.nn.softmax(self.logits) if self.policy_depth > 0 else self.logits
+				logits = input
+				output = tf.nn.softmax(logits) if self.policy_depth > 0 else logits
 			else:
 				policy = []
 				for i in range(self.policy_depth):
 					p = tf.layers.dense(inputs=input, units=self.policy_length, activation=None, kernel_initializer=tf.initializers.variance_scaling)
 					p = tf.expand_dims(p, axis=-1)
 					policy.append(p)
-				self.logits = tf.concat(policy, -1)
-				return tf.contrib.layers.softmax(self.logits)
+				logits = tf.concat(policy, -1)
+				output = tf.contrib.layers.softmax(logits)
+			self.action_batch = self._policy_placeholder("action")
+			self.cross_entropy_batch = self.get_cross_entropy(labels=self.action_batch, logits=logits, softmax=self.policy_depth > 0)
+			self.entropy_batch = self.get_entropy(logits=logits, softmax=self.policy_depth > 0)
+			return output
 
 	def _value_layers(self, input, reuse=False): # Value (output)
 		with tf.variable_scope("base_value{0}".format(self._id), reuse=reuse) as scope:
 			input = tf.layers.dense( inputs=input, units=1, activation=None, kernel_initializer=tf.initializers.variance_scaling )
 			return tf.reshape(input, [-1]) # flatten output
 		
-	def get_cross_entropy(self, onehot_labels, logits):
-		# p
-		a0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
-		ea0 = tf.exp(a0)
-		z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
-		p0 = ea0 / z0
-		# q
-		a1 = onehot_labels - tf.reduce_max(onehot_labels, axis=-1, keepdims=True)
-		ea1 = tf.exp(a1)
-		z1 = tf.reduce_sum(ea1, axis=-1, keepdims=True)
-		p1 = ea1 / z1
-		return tf.reduce_sum( tf.multiply(p1, (tf.log(z0) - a0)), axis=-1)
+	def get_cross_entropy(self, labels, logits, softmax):
+		if softmax: # discrete control
+			return tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
+			# The following alternative is: (i) less numerically stable (since the softmax may compute much larger values) and (ii) less efficient (since some redundant computation would happen in the backprop)
+			# log_batch = tf.log(tf.clip_by_value(logits, 1e-8, 1.0)) # Avoid NaN with clipping when value in tensor becomes zero
+			# return tf.reduce_sum(tf.multiply(log_batch, labels), reduction_indices=1)
+		# else continuous control
+		bn = tf.layers.BatchNormalization(trainable=self._training)
+		batch_layer = bn.apply(logits, training=self._training)
+		mean = bn.moving_mean
+		std = tf.sqrt(bn.moving_variance)
+		return 0.5 * tf.reduce_sum(tf.square((labels - mean) / std), axis=-1) \
+			   + 0.5 * np.log(2.0 * np.pi) * tf.to_float(tf.shape(labels)[-1]) \
+			   + tf.reduce_sum(tf.log(std), axis=-1)
 			
-	def get_entropy(self, logits):
-		a0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
-		ea0 = tf.exp(a0)
-		z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
-		p0 = ea0 / z0
-		return tf.reduce_sum( tf.multiply(p0, (tf.log(z0) - a0)), axis=-1)
+	def get_entropy(self, logits, softmax):
+		if softmax: # discrete control
+			a0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
+			ea0 = tf.exp(a0)
+			z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
+			p0 = ea0 / z0
+			return tf.reduce_sum( tf.multiply(p0, (tf.log(z0) - a0)), axis=-1)
+		# else continuous control
+		bn = tf.layers.BatchNormalization(trainable=True)
+		batch_layer = bn.apply(logits, training=True)
+		std = tf.sqrt(bn.moving_variance)
+		return tf.reduce_sum(tf.log(std) + .5 * np.log(2.0 * np.pi * np.e), axis=-1)
 			
 	def prepare_loss(self):
 		with tf.device(self._device):
 			# Batch placeholders
-			self.action_batch = self._policy_placeholder("action")
 			self.old_policy_batch = self._policy_placeholder("old_policy")
 			self.old_value_batch = self._value_placeholder("old_value")
 			self.advantage_batch = self._value_placeholder("advantage")
 			self.cumulative_reward_batch = self._value_placeholder("cumulative_reward")
-			self.entropy_batch = self.get_entropy(logits=self.logits)
-			self.cross_entropy_batch = tf.losses.softmax_cross_entropy(onehot_labels=self.action_batch, logits=self.logits) if self.policy_depth > 0 else self.get_cross_entropy(onehot_labels=self.action_batch, logits=self.logits)
-			# The following alternative is: (i) less numerically stable (since the softmax may compute much larger values) and (ii) less efficient (since some redundant computation would happen in the backprop)
-			# log_batch = tf.log(tf.clip_by_value(self.logits, 1e-8, 1.0)) # Avoid NaN with clipping when value in tensor becomes zero
-			# return tf.reduce_sum(tf.multiply(log_batch, self.action_batch), reduction_indices=1)
 			self.old_cross_entropy_batch = self._value_placeholder("old_cross_entropy")
 			# Build losses
 			self.policy_loss = PolicyLoss(
@@ -202,9 +207,9 @@ class BaseAC_Network(object):
 			
 	def reward_prediction_loss(self):
 		# reward prediction target. one hot vector
-		self.reward_prediction_target = self._reward_prediction_target_placeholder("reward_prediction_target")
+		self.reward_prediction_labels = self._reward_prediction_target_placeholder("reward_prediction_target")
 		# Reward prediction loss (output)
-		return -self.get_cross_entropy(onehot_labels=self.reward_prediction_target, logits=self._reward_prediction)
+		return -self.get_cross_entropy(labels=self.reward_prediction_labels, logits=self.reward_prediction_logits, softmax=True)
 
 	def bind_sync(self, src_network):
 		src_vars = src_network.get_vars()
@@ -314,6 +319,6 @@ class BaseAC_Network(object):
 		if self.predict_reward:
 			feed_dict.update( {
 				self._reward_prediction_states: reward_prediction_states,
-				self.reward_prediction_target: reward_prediction_target
+				self.reward_prediction_labels: reward_prediction_target
 			} )
 		return feed_dict
