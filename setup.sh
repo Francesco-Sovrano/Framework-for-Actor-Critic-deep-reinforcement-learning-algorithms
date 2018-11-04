3c316c6259dc39be4e1fd8d20072b4128c46081d
Cescoso <cesco.sovrano@gmail.com>
2018-11-04 17:41:35 +0100

    Fix: googletrans stopped working with error 'NoneType' object has no attribute 'group' -> https://stackoverflow.com/questions/52455774/googletrans-stopped-working-with-error-nonetype-object-has-no-attribute-group
    Fix: googletrans stopped working with error 'NoneType' object has no attribute 'group' -> https://stackoverflow.com/questions/52455774/googletrans-stopped-working-with-error-nonetype-object-has-no-attribute-group

--- setup.sh
+++ setup.sh
@@ -22,12 +22,18 @@ pip install pyte
 $MY_PATH/Rogue/build_with_no_monsters.sh
 $MY_PATH/Rogue/build_with_monsters.sh
 # install sentipolc
-pip install googletrans==2.2.0 gensim==3.4.0 validate_email==1.3
+pip install gensim==3.4.0 validate_email==1.3
 pip install nltk==3.2.5 treetaggerwrapper==2.2.4 git+https://github.com/facebookresearch/fastText.git@3e64bf0f5b916532b34be6706c161d7d0a4957a4 # the Moses tokenizer has been removed from nltk 3.3.0!
 pip install emojipy==3.0.5 # https://github.com/emojione/emojione/tree/master/lib/python
 # pip install lxml git+https://github.com/opener-project/VU-sentiment-lexicon.git # this version of VU-sentiment-lexicon is for python2 only
 pip install lxml==4.2.1 git+https://github.com/Francesco-Sovrano/VU-sentiment-lexicon.git
+
 cd ./.env
+# install googletrans: https://stackoverflow.com/questions/52455774/googletrans-stopped-working-with-error-nonetype-object-has-no-attribute-group
+git clone https://github.com/BoseCorp/py-googletrans.git
+cd ./py-googletrans
+python setup.py install
+cd ..
 # install treetagger
 if [ ! -d "treetagger" ]; then
 	cp -r ../Sentipolc/database/treetagger ./treetagger
